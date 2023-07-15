4d6f332faa03a42b62d8008b7b223f19fd4b9b86
Jaggehn <jaggehns@gmail.com>
2023-07-16 07:15:39 +0800

    Schema & Types

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
-//server setup
+// types
+import { typeDefs } from "./schema";
+
+// server setup
 const server = new ApolloServer({
-  //typeDefs
-  //resolvers
+  typeDefs,
+  // resolvers
 });
 
 const { url } = await startStandaloneServer(server, {
